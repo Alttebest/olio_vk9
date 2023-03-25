967d97bd63837c4965bb76564b4a42e9259d2f29
Alttebest <Aleksis.kulta@gmail.com>
2023-03-25 19:14:45 +0200

    olio viikko 9

--- app/src/main/java/com/example/vk9/UserViewHolder.java
+++ app/src/main/java/com/example/vk9/UserViewHolder.java
@@ -3,7 +3,6 @@ package com.example.vk9;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
